ab3d700503491f869f5efc193376763dd562cb01
Eugene Bondar <bondar.e@nixsolutions.com>
2018-12-06 17:49:24 +0200

    tasks ended

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import List from './List';
+import TextTrimmer from './TextTrimmer';
+import heroes from '../utils/heroes';
 
-const formatName = user => `${user.firstName} ${user.lastName}`;
+function renderItem(node, item) {
+	if (node === 'li') {
+		return (
+			<li key={item.id}>
+				<div>Hero - {item.hero}</div>
+				<img src={item.avatar} />
+				<div>Name - {item.name}</div>
+				<TextTrimmer text={item.description} />
+			</li>
+		);
+	} else {
+		return (
+			<div key={item.id}>
+				<div>Hero - {item.hero}</div>
+				<img src={item.avatar} />
+				<div>Name - {item.name}</div>
+				<TextTrimmer text={item.description} />
+			</div>
+		);
+	}
+}
 
-const user = {
-	firstName: 'John',
-	lastName: 'Doe'
-};
-
-const Home = () => (
-	<div>
-		<h1>Hello, {formatName(user)}!</h1>
-	</div>
-);
+class Home extends React.Component {
+	render() {
+		return (
+			<div>
+				<List options={heroes} parentNode={'ul'} renderItem={renderItem} />
+			</div>
+		);
+	}
+}
 
 export default Home;
